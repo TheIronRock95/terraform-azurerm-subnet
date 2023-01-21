703156443d1797b18d4d863fd23092ed8b76fd59
TheIronRock95 <58424695+TheIronRock95@users.noreply.github.com>
2023-01-22 00:30:25 +0100

    fix: locals.tf

--- locals.tf
+++ locals.tf
@@ -1,3 +1,4 @@
 locals {
-
+  network_security_group_id = format("/subscriptions/%s/resourceGroups/%s/providers/Microsoft.Network/networkSecurityGroups/%s", data.azurerm_subscription.current.subscription_id, var.nsg_resource_group_name, coalesce(var.network_security_group_name, "fake"))
+  subnet_id = "/subscriptions/${data.azurerm_subscription.current.subscription_id}/resourceGroups/${var.resource_group_name}/providers/Microsoft.Network/virtualNetworks/${var.virtual_network_name}/subnets/${coalesce(var.subnet_name)}"
 }
