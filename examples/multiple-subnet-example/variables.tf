edcafdc4178dda58c1e44ca0c6f56565eee56113
TheIronRock95 <58424695+TheIronRock95@users.noreply.github.com>
2023-01-22 00:17:01 +0100

    feat: update examples

--- examples/multiple-subnet-example/variables.tf
+++ examples/multiple-subnet-example/variables.tf
@@ -6,25 +6,17 @@ variable "subnet_config" {
   description = "Config for multiple subnet"
   type = map
   default = {
-    "DMN-WE-P-SNET-TERRAFORM" = {
-        subnet_name = "DMN-WE-P-SNET-TERRAFORM"
+    "multiple-subnet-example" = {
+        subnet_name = "myfirstsubnet"
         address_prefixes = ["10.1.0.0/27"]
-    }
-    "DMN-WE-A-SNET-TERRAFORM" = {
-        subnet_name = "DMN-WE-A-SNET-TERRAFORM"
+        network_security_group_name = "myfirstnsg"
+        nsg_resource_group_name = "myfirstrg"
+    },
+    "multiple-subnet-example2" = {
+        subnet_name = "mysecondsubnet"
         address_prefixes = ["10.1.0.32/27"]
+        network_security_group_name = "mysecondnsg"
+        nsg_resource_group_name = "mysecondrg"
     },
-    "DMN-WE-T-SNET-TERRAFORM" = {
-        subnet_name = "DMN-WE-T-SNET-TERRAFORM"
-        address_prefixes = ["10.1.0.64/27"]
-    },
-    "DMN-WE-D-SNET-TERRAFORM" = {
-        subnet_name = "DMN-WE-D-SNET-TERRAFORM"
-        address_prefixes = ["10.1.0.96/27"]
-    },
-      "DMN-WE-MGMT-SNET-TERRAFORM" = {
-        subnet_name = "DMN-WE-MGMT-SNET-TERRAFORM"
-        address_prefixes = ["10.1.0.128/25"]
-    }
   }
 }
