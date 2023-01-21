6ef922d5c232a0bf792f854663f95114ef446120
TheIronRock95 <58424695+TheIronRock95@users.noreply.github.com>
2023-01-22 00:22:49 +0100

    style: terraform fmt

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ resource "azurerm_subnet" "this" {
 resource "azurerm_subnet_network_security_group_association" "this" {
   count = var.network_security_group_name == null ? 0 : 1
 
-  subnet_id                 = local.subnet_id 
+  subnet_id                 = local.subnet_id
   network_security_group_id = local.network_security_group_id
 
   depends_on = [
